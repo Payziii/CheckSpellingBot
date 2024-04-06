bfb5a88801dab2021c42285cb5d656751f46ebe1
Payziii <velmikhail123@gmail.com>
2024-04-06 14:49:45 +0500

    Готовая версия бота

--- bot.js
+++ bot.js
@@ -10,11 +10,15 @@ bot.use(conversations());
 bot.use(createConversation(checker));
 
 bot.command("start", async (ctx) => {
-  ctx.reply("Список команд\n\n/text")
+  ctx.reply("Список команд\n\n/text - Проверить текст на наличие ошибок\n/info - Информация о боте")
 });
 
 bot.command("text", async (ctx) => {
   await ctx.conversation.enter("check");
+});
+
+bot.command("info", async (ctx) => {
+  ctx.reply("Создатель бота: @Payziii\nИсходный код: https://github.com/Payziii/CheckSpellingBot")
 });
 
 bot.on("callback_query:data", async (ctx) => {
--- checker.js
+++ checker.js
@@ -3,10 +3,12 @@ const { setTimeout } = require("timers/promises");
 
 module.exports = async function check(conversation, ctx) {
 
-    await ctx.reply("Введите текст (от 100 до 1500 символов)")
+    await ctx.reply("Введите текст (от 100 до 15000 символов)")
 
     let text = await conversation.wait();
     text = text.message.text;
+
+    if(text.length < 100 ||  text.length > 15000) return ctx.reply("В вашем тексте меньше 100 или больше 15000 символов!")
 
     const body = {
         "userkey": process.env.TEXT_TOKEN,
@@ -14,7 +16,7 @@ module.exports = async function check(conversation, ctx) {
     };
 
     const answer = await Data.sendResponse(body);
-    if(answer.error_desc) return ctx.reply("Ошибка:" + answer.error_desc);
+    if(answer.error_desc) return ctx.reply("Ошибка: " + answer.error_desc);
 
     const uid = answer.text_uid;
 
@@ -26,7 +28,9 @@ module.exports = async function check(conversation, ctx) {
         "jsonvisible": "detail"
     };
     const answer2 = await Data.sendResponse(body2);
-    console.log(answer2)
     answ = JSON.parse(answer2.spell_check)
-    ctx.reply(answ[0].error_type + "\n" + answ[0].replacements)
+    for(let i=0; i<answ.length; i++) {
+        ctx.reply(`Ошибка ${i+1}.\n\n${answ[i].error_type}: ${answ[i].reason}\n\nВарианты замены:\n<b>${answ[i].error_text}</b> ---> <b>${answ[i].replacements}</b>`,
+        { parse_mode: "HTML" })
+    }
 }
